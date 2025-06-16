40a1399f68d2201839f103e988b40aad03a7ac1c
Ptitlu42 <lucas.beyer@gmx.fr>
2025-06-16 11:52:14 +0200

    Add mochawesome reporting to Cypress configuration and update GitHub Actions workflow for report generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   'projectId': '4b7344',
-  e2e: {},
+  e2e: {
+    video: true,
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome',
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  },
 }
